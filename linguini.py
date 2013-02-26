c87b77caa35055c955cacf6a64ffdfcd15d75dba
dtk <dtk@gmx.de>
2013-02-26 01:27:35 +0100

    Add `linguini` script
    Currently `linguini` is able to process input from STDIN and the primary
    clipboard. He tracks his state via a primitive state maschine and collects
    required data in a dedicated data structure (cf `Recipe.py`). When the data is
    complete, he renders simple nested template snippets to generate a LaTeX
    `\recipe` environment.

---
+++ linguini.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+
+import gtk
+import gobject
+import sys
+import signal
+import logging
+
+from recipe import Recipe, Ingredient
+
+
+class State(object):
+    (TITLE, CREATOR, NUM_DISHES, PREP_TIME, INGREDIENTS,
+     INGREDIENT, AMOUNT, UNIT, PROCESSING,
+     DONE) = range(10)
+
+    _prompts = { TITLE: "What is the title of the recipe?",
+                 CREATOR: "Who created this recipe?",
+                 NUM_DISHES: "How many dishes does this recipe serve?",
+                 PREP_TIME: "How long does it take to prepare and cook this recipe?",
+                 INGREDIENTS: "Please enter an ingredient:",
+                 INGREDIENT: "Which ingredient do you want to add? (Ctrl+C to abort)",
+                 AMOUNT: "How much do you want to add of this ingredient?",
+                 UNIT: "What is the unit of this ingredient?",
+                 PROCESSING: "What should be done with the ingredients?", }
+
+    def __init__(self):
+        self._current = State.TITLE
+
+    @property
+    def current(self):
+        return self._current
+
+    @current.setter
+    def current(self, state):
+        self._current = state
+
+    def prompt(self):
+        print(State._prompts[self._current])
+
+    def advance(self):
+        self._current += 1
+        self.prompt()
+
+
+class KitchenHand(object):
+    SIGNAL_NEW_CLIPBOARD_CONTENT = 'owner-change'
+
+    def __init__(self):
+        self._recipe = Recipe()
+        self._state = State()
+
+        self._watch_inputs()
+        signal.signal(signal.SIGINT, self._handle_SIGINT)
+
+        self._state.prompt()
+
+    def _watch_inputs(self):
+        self._watch_clipboard()
+        self._watch_stdin()
+
+    def _watch_clipboard(self):
+        self.clipboard = gtk.clipboard_get(gtk.gdk.SELECTION_PRIMARY)
+        self.clipboard.connect(self.SIGNAL_NEW_CLIPBOARD_CONTENT,
+                               self._handle_clipboard_content)
+
+    def _watch_stdin(self):
+        gobject.io_add_watch(sys.stdin, gobject.IO_IN,
+                             self._handle_stdin_content)
+
+    def _handle_clipboard_content(self, clipboard, event):
+        selection = clipboard.wait_for_text()
+        print selection    # provide feedback for the user
+        self._process_input(selection)
+
+    def _handle_stdin_content(self, stdin, condition):
+        value = stdin.readline()
+        self._process_input(value)
+        return True
+
+    def _handle_SIGINT(self, signal, frame):
+        logging.debug("caught SIGINT")
+        if self._state.current in [State.INGREDIENTS,
+                                   State.INGREDIENT,
+                                   State.AMOUNT,
+                                   State.UNIT,
+                                   State.PROCESSING]:
+            self._state.current = State.DONE
+            self._process_input(None)
+        else:
+            logging.debug("Quitting while in state '{}'".format(
+                self._state.current))
+            gtk.main_quit()
+
+    def _write(self):
+        #TODO: Dummy implementation
+        print self._recipe.render()
+
+    def _process_input(self, value):
+        if value: value = value.strip()
+        logging.debug("processing input '{}' in state {}".format(
+            value, self._state.current))
+
+        if self._state.current == State.TITLE:
+            self._recipe.title = value
+            self._state.advance()
+        elif self._state.current == State.CREATOR:
+            self._recipe.creator = value
+            self._state.advance()
+        elif self._state.current == State.NUM_DISHES:
+            self._recipe.num_dishes = value
+            self._state.advance()
+        elif self._state.current == State.PREP_TIME:
+            self._recipe.prep_time = value
+            self._state.current = State.INGREDIENTS
+            self._process_input(None)
+        elif self._state.current == State.INGREDIENTS:
+            self._ingredient = Ingredient()
+            self._state.prompt()
+            self._state.advance()
+        elif self._state.current == State.INGREDIENT:
+            self._ingredient.description = value
+            self._state.advance()
+        elif self._state.current == State.AMOUNT:
+            self._ingredient.amount = value
+            self._state.advance()
+        elif self._state.current == State.UNIT:
+            self._ingredient.unit = value
+            self._state.advance()
+        elif self._state.current == State.PROCESSING:
+            self._ingredient.processing = value
+            self._recipe.add(self._ingredient)
+            self._state.current = State.INGREDIENTS
+            self._process_input(None)
+        else:
+            self._write()
+            self._exit()
+
+    def _exit(self):
+        print "We're done!"
+        sys.exit()
+
+
+
+if __name__ == '__main__':
+
+    #logging.basicConfig(level=logging.DEBUG)
+
+    hand = KitchenHand()
+
+    try:
+        gtk.main()
+    except KeyboardInterrupt as err:
+        pass
---
+++ recipe.py
@@ -0,0 +1,113 @@
+from string import Template
+import logging
+
+
+class Recipe(object):
+
+    _recipe_format = \
+"""
+\\begin{recipe}$header
+$ingredients
+\end{recipe}
+"""
+
+    _header_format = '{$title \creator{$creator}}{$num_dishes}{$prep_time}'
+
+    def __init__(self):
+        self._ingredients = []
+
+    @property
+    def title(self): pass
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def creator(self): pass
+
+    @creator.setter
+    def creator(self, creator):
+        self._creator = creator
+
+    @property
+    def num_dishes(self): pass
+
+    @num_dishes.setter
+    def num_dishes(self, num_dishes):
+        self._num_dishes = num_dishes
+
+    @property
+    def prep_time(self): pass
+
+    @prep_time.setter
+    def prep_time(self, prep_time):
+        self._prep_time = prep_time
+
+    def add(self, ingredient):
+        self._ingredients.append(ingredient)
+
+    def render(self):
+        logging.debug("Rendering recipe")
+        header = self._render_header()
+        ingredients = self._render_ingredients()
+
+        template = Template(Recipe._recipe_format)
+        return template.substitute(header=header, ingredients=ingredients)
+
+    def _render_header(self):
+        logging.debug("Rendering header")
+        template = Template(Recipe._header_format)
+        return template.substitute(title=self._title,
+                                   creator=self._creator,
+                                   num_dishes=self._num_dishes,
+                                   prep_time=self._prep_time)
+
+    def _render_ingredients(self):
+        logging.debug("Rendering ingredients")
+        ingredients = ""
+        for ingredient in self._ingredients:
+            ingredients += ingredient.render()
+        return ingredients
+
+
+class Ingredient(object):
+
+    _format = """    \ingredient[$amount]{$unit}{$description}
+        $processing
+"""
+
+    @property
+    def amount(self): pass
+
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
+
+    @property
+    def unit(self): pass
+
+    @unit.setter
+    def unit(self, unit):
+        self._unit = unit
+
+    @property
+    def description(self): pass
+
+    @description.setter
+    def description(self, description):
+        self._description = description
+
+    @property
+    def processing(self): pass
+
+    @processing.setter
+    def processing(self, processing):
+        self._processing = processing
+
+    def render(self):
+        template = Template(Ingredient._format)
+        return template.substitute(amount=self._amount,
+                                   unit=self._unit,
+                                   description=self._description,
+                                   processing=self._processing)
